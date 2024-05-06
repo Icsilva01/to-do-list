200e46050d46144686b71635bd3cba5f6218d4c7
Icaro Silva <107263783+Icsilva01@users.noreply.github.com>
2024-05-06 15:01:14 -0300

    to-do-list ok

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Checkbox } from "./components/ui/checkbox";
 import { Button } from "./components/ui/button";
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
+import { MdOutlineDeleteForever } from "react-icons/md";
+import { IoIosAddCircleOutline } from "react-icons/io";
 import {
   Dialog,
   DialogContent,
@@ -24,7 +26,7 @@ export function App() {
   const [checkboxes, setCheckboxes] = useState<Checkbox[]>([]);
   const [inputValue, setInputValue] = useState("");
 
-  const handleCheckboxChange = (id: string, checked: boolean) => {
+  const handleCheckboxChange = (id: string, checked: any) => {
     setCheckboxes((checkboxes) =>
       checkboxes.map((checkbox) =>
         checkbox.id === id ? { ...checkbox, checked } : checkbox
@@ -35,60 +37,87 @@ export function App() {
   const handleSaveChanges = () => {
     setCheckboxes([
       ...checkboxes,
-      { id: `checkbox-${checkboxes.length + 1}`, label: inputValue, checked: false },
+      {
+        id: `checkbox-${checkboxes.length + 1}`,
+        label: inputValue,
+        checked: false,
+      },
     ]);
     setInputValue("");
-
   };
 
+  const deleteTask = (id: string) => {
+    setCheckboxes(checkboxes.filter((checkbox) => checkbox.id !== id));
+  };
 
   return (
     <div className="flex p-6 mx-auto flex-col justify-center items-center gap-2 h-screen bg-slate-300">
-    <Card className="p-4 w-96 h-56">
-    <h1 className="text-2xl text-center">TaskDo</h1>
-    <Dialog>
-      <DialogTrigger asChild className="flex gap-2">
-        <Button>Adicionar nova tarefa</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Adicionar nova tarefa</DialogTitle>
-          <DialogDescription>
-            Adicione uma nova tarefa para ser realizada.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-center">
-              Tarefa
-            </Label>
-            <Input id="name" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className="col-span-3" />
+      <Card className="p-4 w-96 h-min-56">
+        <h1 className="text-2xl text-center">TaskDo</h1>
+        <Dialog>
+          <DialogTrigger asChild className="flex gap-2">
+            <Button variant={"ghost"} className="text-purple-950">
+              <IoIosAddCircleOutline className="size-5"/>
+              Adicionar nova tarefa
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Adicionar nova tarefa</DialogTitle>
+              <DialogDescription>
+                Adicione uma nova tarefa para ser realizada.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-center">
+                  Tarefa
+                </Label>
+                <Input
+                  id="name"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button
+                  type="submit"
+                  onClick={handleSaveChanges}
+                  disabled={!inputValue}
+                >
+                  Salvar
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+        {checkboxes.map((checkbox) => (
+          <div className="flex space-x-2 p-2 items-center" key={checkbox.id}>
+            <MdOutlineDeleteForever
+              className={`${checkbox.checked ? "cursor-pointer" : "cursor-auto"} size-5`}
+              onClick={
+                checkbox.checked ? () => deleteTask(checkbox.id) : undefined
+              }
+            />
+            <Checkbox
+              id={checkbox.id}
+              checked={checkbox.checked}
+              onCheckedChange={(checked) =>
+                handleCheckboxChange(checkbox.id, checked)
+              }
+            />
+            <label
+              htmlFor={checkbox.id}
+              className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${checkbox.checked ? "line-through" : ""}`}
+            >
+              {checkbox.label}
+            </label>
           </div>
-        </div>
-        <DialogFooter>
-          <DialogClose asChild>
-          <Button type="submit" onClick={handleSaveChanges}>Salvar</Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-    {checkboxes.map((checkbox) => (
-      <div className="flex space-x-2 p-2" key={checkbox.id}>
-        <Checkbox
-          id={checkbox.id}
-          checked={checkbox.checked}
-          //@ts-expect-error
-          onCheckedChange={(checked) => handleCheckboxChange(checkbox.id, checked)}
-        />
-        <label
-          htmlFor={checkbox.id}
-          className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${checkbox.checked ? "line-through" : ""}`}
-        >
-          {checkbox.label}
-        </label>
-      </div>
-    ))}
-    </Card>
-  </div>
+        ))}
+      </Card>
+    </div>
   );
 }
